c9430ec95840181e1f849304aa6db0f6d3f194b3
narsimha777 <kandinarsimhareddy09@gmail.com>
2024-05-22 11:51:40 +0530

    addedoneproblem in Array hard

--- Arrays-(Hard).cpp
+++ Arrays-(Hard).cpp
@@ -319,6 +319,25 @@ int reversePairs(vector<int>arr){
     return mergeSortRP(arr, 0, arr.size()-1);
 }
 
+//max product in an array
+
+int maxProduct(vector<int>arr, int n){
+    int pre=1;
+    int suff=1;
+    int ans = INT_MIN;
+    for(int i=0;i<n;i++){
+        if(pre==0){
+            pre = 1;
+        }
+        if(suff==0){
+            suff =1;
+        }
+        pre*=arr[i];
+        suff*=arr[n-i-1];
+        ans= max(ans, max(pre,suff));
+    }
+    return ans;
+}
 
 int main(){
     
@@ -405,7 +424,9 @@ int main(){
     // int res = inversionCount(arr);
     // cout<<"Result: "<<res;
     
-    int res = reversePairs(arr);
-    cout<<"Result: "<<res;
+    // int res = reversePairs(arr);
+    // cout<<"Result: "<<res;
     
+    int res = maxProduct(arr, n);
+    cout<<"Answer: "<<res;
 }
